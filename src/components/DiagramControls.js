af5fb45151e744e25a54189a8819bef8fc2d8885
deepsn458 <deepaknarayan458@gmail.com>
2026-03-03 01:27:18 -0600

    initial integrated diagram controls and live readout table

--- src/components/DiagramControls.js
+++ src/components/DiagramControls.js
@@ -4,31 +4,54 @@ import Sphinx_Diagram from '../diagrams/sphinx_diagram.svg';
 import Proxima_Diagram from '../diagrams/proxima_diagram.svg';
 import { GaugeReading } from './GaugeReading';
 import { Valve } from './Valve';
-import "../styles/proxima_diagram.css"
-import "../styles/sphinx_diagram.css"
-
-
-
+import proxima_configs from '../configs/proxima_configs.json';
+import sphinx_configs from '../configs/sphinx_configs.json';
+import "../styles/proxima_diagram.css";
+import "../styles/sphinx_diagram.css";
 
 class DiagramControls extends React.Component {
     constructor(props){
         super(props);
         this.props = props;
         this.interface = props.interface.current;
-        this.gauge_refs = {
-            load_cell: React.createRef(),
-            feed_line_pt: React.createRef(),
-            cc_pt: React.createRef(),
-            injector_pt: React.createRef(),
-            ox_tank_pt: React.createRef()
-        };
-        this.valve_refs = {
-            ox_fill: React.createRef(),
-            ground_vent: React.createRef(),
-            engine_vent: React.createRef(),
-            ops_pneumatic: React.createRef()
-        };
+
+        this.config = this.getActiveConfig();
+        this.gauge_keys = this.getGaugeKeys(this.config);
+        this.driver_keys = this.getDriverKeys(this.config);
+
+        this.gauge_refs = this.gauge_keys.reduce((acc, key) => {
+            acc[key] = React.createRef();
+            return acc;
+        }, {});
+
+        this.valve_refs = this.driver_keys.reduce((acc, key) => {
+            acc[key] = React.createRef();
+            return acc;
+        }, {});
+
+        this.gauge_slots = ["feed_line_pt", "cc_pt", "injector_pt", "ox_tank_pt", "load_cell"];
+        this.valve_slots = ["ox_fill_valve", "ground_vent_valve", "nitrogen_purge_valve", "engine_isolation_valve"];
     }
+
+    getActiveConfig = () => {
+        const path = window.location.pathname.toLowerCase();
+        if (path.includes("proximamaindisplay")) {
+            return proxima_configs;
+        }
+        return sphinx_configs;
+    }
+
+    getGaugeKeys = (config) => {
+        const pts = Object.values(config?.sensor_ids?.pts || {});
+        const lcs = Object.values(config?.sensor_ids?.lcs || {});
+        return [...new Set([...pts, ...lcs])];
+    }
+
+    getDriverKeys = (config) => {
+        return Object.values(config?.sensor_ids?.drivers || {}).filter((driver) => driver !== "ignition");
+    }
+
+
 
     update(dataManager) {
         const data = dataManager.modifiedDataset;
@@ -36,45 +59,81 @@ class DiagramControls extends React.Component {
         if (data === null || data.length === 0) {
             return;
         }
+
         for (const [sensor, val] of Object.entries(data[data.length - 1])) {
             if (sensor in this.gauge_refs && this.gauge_refs[sensor].current !== null) {
                 this.gauge_refs[sensor].current.setValue(val);
             }
         }
+
         const valves = dataManager.valve_states;
-        this.valve_refs.ox_fill.current.setOpen(valves.ox_fill);
-        this.valve_refs.ground_vent.current.setOpen(valves.ground_vent);
-        this.valve_refs.ops_pneumatic.current.setOpen(valves.ops_pneumatic);
-        this.valve_refs.engine_vent.current.setOpen(valves.engine_vent);
+        for (const driver of this.driver_keys) {
+            if (this.valve_refs[driver]?.current) {
+                this.valve_refs[driver].current.setOpen(Boolean(valves[driver]));
+            }
+        }
     }
 
     sendDriverCommand(driver_name) {
         return (state) => {
-            this.interface?.sendDriverUpdate(driver_name, state)
-        }
+            this.interface?.sendDriverUpdate(driver_name, state);
+        };
     }
 
     componentDidMount() {
-        this.valve_refs.ground_vent.current.setOpen(true);
+        if (this.valve_refs.ground_vent?.current) {
+            this.valve_refs.ground_vent.current.setOpen(true);
+        }
+    }
+
+    renderGauge = (sensor, index) => {
+        const slotId = this.gauge_slots[index];
+        const wrapperProps = { id: slotId };
+
+        return (
+            <div key={sensor} {...wrapperProps}>
+                <GaugeReading
+                    title={sensor}
+                    size={sensor === "load_cell" ? 90 : 70}
+                    ref={this.gauge_refs[sensor]}
+                />
+            </div>
+        );
+    }
+
+    renderValve = (driver, index) => {
+        const slotId = this.valve_slots[index];
+        const wrapperProps = slotId
+            ? { id: slotId }
+            : { style: { position: "absolute", right: 20, top: 20 + (index - this.valve_slots.length) * 95 } };
+
+        return (
+            <div key={driver} {...wrapperProps}>
+                <Valve
+                    title={driver}
+                    toggle_cmd={this.sendDriverCommand(driver)}
+                    opposite={driver === "ground_vent"}
+                    text_bottom={driver === "ground_vent"}
+                    text_margin={driver === "ground_vent" ? "22px" : 0}
+                    ref={this.valve_refs[driver]}
+                />
+            </div>
+        );
     }
 
     render() {
+        const path = window.location.pathname.toLowerCase();
+        const diagram = path.includes("proximamaindisplay") ? Proxima_Diagram : Sphinx_Diagram;
+
         return (
             <div style={{position: "relative"}}>
-                <img src={Sphinx_Diagram} alt="Diagram" style={{height: 550, width: 1000}}></img>
+                <img src={diagram} alt="Diagram" style={{height: 550, width: 1000}}></img>
                 <Box sx={{textAlign: "center"}} id="diagram-box">
-                    {/* <div id="feed_line_pt"><GaugeReading title="Feed Line PT" ref={this.gauge_refs.feed_line_pt}/></div>
-                    <div id="cc_pt"><GaugeReading title="CC PT" ref={this.gauge_refs.cc_pt}/></div>
-        <div id="injector_pt"><GaugeReading title="Inj. PT" ref={this.gauge_refs.injector_pt}/></div> */}
-                    <div id="ox_tank_pt"><GaugeReading title="Ox. PT" ref={this.gauge_refs.ox_tank_pt}/></div>
-                    <div id="load_cell"><GaugeReading title="Load Cell" size={90} ref={this.gauge_refs.load_cell}/></div>
-                    <div id="ox_fill_valve"><Valve title="Fill Valve" toggle_cmd={this.sendDriverCommand("ox_fill")} ref={this.valve_refs.ox_fill}/></div>
-                    <div id="ground_vent_valve"><Valve title="Vehicle Valve 1" toggle_cmd={this.sendDriverCommand("ground_vent")} opposite={true} ref={this.valve_refs.ground_vent} text_bottom text_margin="22px"/></div>
-                    <div id="nitrogen_purge_valve"><Valve title="Vehicle Valve 2" toggle_cmd={this.sendDriverCommand("engine_vent")} ref={this.valve_refs.engine_vent}/></div>
-                    <div id="engine_isolation_valve"><Valve title="Purge Valve" toggle_cmd={this.sendDriverCommand("ops_pneumatic")} ref={this.valve_refs.ops_pneumatic}/></div>
+                    {this.gauge_keys.map(this.renderGauge)}
+                    {this.driver_keys.map(this.renderValve)}
                 </Box>
             </div>
-        )
+        );
     }
 }
 
--- src/components/LiveReadoutTable.js
+++ src/components/LiveReadoutTable.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -12,68 +12,102 @@ import sphinx_configs from '../configs/sphinx_configs.json';
 class LiveReadoutTable extends React.Component {
     constructor(props) {
         super(props);
-        var sensors = 
+        this.config = this.getActiveConfig();
+        this.sensorKeys = this.getSensorKeys(this.config);
+
+        const defaults = this.buildDefaults(this.sensorKeys);
         this.state = {
             displayMode: "rawData",
-            data: {
-                load_cell: 0.0,
-                feed_line_pt: 0.0,
-                cc_pt: 0.0,
-                injector_pt: 0.0,
-                ox_tank_pt: 0.0,
-            },
-            unit: {
-                load_cell: "lbf",
-                feed_line_pt: "psig",
-                cc_pt: "psig",
-                injector_pt: "psig",
-                ox_tank_pt: "psig",
-            },
-            display_unit: {
-              load_cell: "lbf",
-              feed_line_pt: "psig",
-              cc_pt: "psig",
-              injector_pt: "psig",
-              ox_tank_pt: "psig",
-            },
-            precision: { // Nums past decimal point
-                load_cell: 2,
-                feed_line_pt: 2,
-                cc_pt: 2,
-                injector_pt: 2,
-                ox_tank_pt: 2,
+            data: defaults.data,
+            unit: defaults.unit,
+            display_unit: defaults.display_unit,
+            precision: defaults.precision,
+        };
+    }
+
+    getActiveConfig = () => {
+        const path = window.location.pathname.toLowerCase();
+        if (path.includes("proximamaindisplay")) {
+            return proxima_configs;
+        }
+        return sphinx_configs;
+    }
+
+    getSensorKeys = (config) => {
+        const sensorGroups = config?.sensor_ids || {};
+        const keys = [];
+        for (const [group, mappings] of Object.entries(sensorGroups)) {
+            if (group === "drivers") {
+                continue;
+            }
+            for (const key of Object.values(mappings || {})) {
+                keys.push(key);
             }
         }
+        return [...new Set(keys)];
+    }
+
+    inferUnit = (sensorKey) => {
+        if (sensorKey === "load_cell") {
+            return "lbf";
+        }
+        if (sensorKey.includes("_pt") || sensorKey === "ox_pt" || sensorKey === "ops_pt") {
+            return "psig";
+        }
+        return "";
+    }
+
+    buildDefaults = (sensorKeys) => {
+        const data = {};
+        const unit = {};
+        const display_unit = {};
+        const precision = {};
+
+        for (const key of sensorKeys) {
+            data[key] = 0.0;
+            unit[key] = this.inferUnit(key);
+            display_unit[key] = unit[key];
+            precision[key] = 2;
+        }
+
+        return { data, unit, display_unit, precision };
     }
 
     update(dataManager) {
-      const newData = dataManager.modifiedDataset[dataManager.modifiedDataset.length - 1];
-      const rateOfChangeUnits = {}
-      for (const [key, value] of Object.entries(this.state.unit)) {
-        rateOfChangeUnits[key] = value + "/s";
+      const dataset = dataManager.modifiedDataset;
+      if (!dataset || dataset.length === 0) {
+        return;
       }
-      this.setState(
-        {
-          displayMode: dataManager.getDisplayMode(),
-          data: {
-            load_cell: newData.load_cell,
-            feed_line_pt: newData.feed_line_pt,
-            cc_pt: newData.cc_pt,
-            injector_pt: newData.injector_pt,
-            ox_tank_pt: newData.ox_tank_pt,
-          },
-          display_unit: ((dataManager.displayMode === "rateOfChange") ? rateOfChangeUnits : this.state.unit)
-        }
-      );
+
+      const newData = dataset[dataset.length - 1];
+      const rateOfChangeUnits = {};
+      for (const [key, value] of Object.entries(this.state.unit)) {
+        rateOfChangeUnits[key] = value ? value + "/s" : "";
+      }
+
+      const nextData = {};
+      for (const key of this.sensorKeys) {
+        nextData[key] = newData[key];
+      }
+
+      this.setState({
+        displayMode: dataManager.getDisplayMode(),
+        data: nextData,
+        display_unit: ((dataManager.displayMode === "rateOfChange") ? rateOfChangeUnits : this.state.unit)
+      });
     }
 
     render() {
         const rows = [];
-        for (const [key, value] of Object.entries(this.state.data)) {
+        for (const key of this.sensorKeys) {
+          const value = this.state.data[key];
           if (value === undefined || value === null || isNaN(value)) continue;
-          rows.push({sensor: key, value: value.toFixed(this.state.precision[key]) + " " + this.state.display_unit[key]});
+          const unit = this.state.display_unit[key] ? ` ${this.state.display_unit[key]}` : "";
+          rows.push({sensor: key, value: value.toFixed(this.state.precision[key]) + unit});
         }
-        return (<TableContainer component={Paper}>
+
+        return (
+          <TableContainer component={Paper}>
             <Table aria-label="sensor table">
               <TableHead>
                 <TableRow sx={{ 'td, th': { border: 1, fontSize: 20 } }}>
@@ -88,13 +122,14 @@ class LiveReadoutTable extends React.Component {
                     sx={{ 'td, th': { border: 1, fontSize: 16 } }}
                   >
                     <TableCell component="th" scope="row">{row.sensor}</TableCell>
-                    <TableCell >{row.value}</TableCell>
+                    <TableCell>{row.value}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
-          </TableContainer>);
+          </TableContainer>
+        );
     }
 }
 
-export { LiveReadoutTable }
+export { LiveReadoutTable };
